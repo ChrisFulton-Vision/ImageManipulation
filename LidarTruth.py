e85d27e194ce2fbbcaf081a8440b91353c2955c0
Chris Fulton <fultonc742@gmail.com>
2025-09-06 11:10:34 -0400

    Updated LiDAR Points for MOCAP Capture

--- LidarTruth.py
+++ LidarTruth.py
@@ -10,38 +10,38 @@ class TruthPoints:
         self.saveToCache()
 
     def selectTruthPoints(self):
-        # 0
-        self.truthPoints['0'] = np.array([5.25967, 2.52404, -1.01714])
-        # 1
-        self.truthPoints['1'] = np.array([2.90982, 2.45965, -0.60117])
-        # 4
-        self.truthPoints['4'] = np.array([1.02611, -0.09072, -1.04306])
-        # 5
-        self.truthPoints['5'] = np.array([1.71335, -0.48700, -0.66432])
-        # 6
-        self.truthPoints['6'] = np.array([0.62843, 1.60848, -0.65310])
-        # 7
-        self.truthPoints['7'] = np.array([3.51041, 3.23543, -0.80128])
-        # 8
-        self.truthPoints['8'] = np.array([4.11462, 3.41798, -0.24962])
-        # 9
-        self.truthPoints['9'] = np.array([-0.37882, -0.33134, -0.64973])
-        # 10
-        self.truthPoints['10'] = np.array([1.38713, 0.46928, -0.63064])
-        # 12
-        self.truthPoints['12'] = np.array([2.72211, 4.00315, -0.59416])
-        # 17
-        self.truthPoints['17'] = np.array([1.35135, -0.99257, -0.41729])
-        # 18
-        self.truthPoints['18'] = np.array([4.00388, 4.02418, 0.08610])
-        # 19
-        self.truthPoints['19'] = np.array([1.15202, 1.01983, -0.72926])
-        # 21
-        self.truthPoints['21'] = np.array([4.09984, 1.55906, -0.58504])
-        # 22
-        self.truthPoints['22'] = np.array([2.52925, 1.73592, -0.97918])
-        # 23
-        self.truthPoints['23'] = np.array([2.15135, 3.04855, -0.73227])
+        # CALSPAN 2024 Dec AprilTags against LIDAR
+        # self.truthPoints['0'] = np.array([5.25967, 2.52404, -1.01714])
+        # self.truthPoints['1'] = np.array([2.90982, 2.45965, -0.60117])
+        # self.truthPoints['4'] = np.array([1.02611, -0.09072, -1.04306])
+        # self.truthPoints['5'] = np.array([1.71335, -0.48700, -0.66432])
+        # self.truthPoints['6'] = np.array([0.62843, 1.60848, -0.65310])
+        # self.truthPoints['7'] = np.array([3.51041, 3.23543, -0.80128])
+        # self.truthPoints['8'] = np.array([4.11462, 3.41798, -0.24962])
+        # self.truthPoints['9'] = np.array([-0.37882, -0.33134, -0.64973])
+        # self.truthPoints['10'] = np.array([1.38713, 0.46928, -0.63064])
+        # self.truthPoints['12'] = np.array([2.72211, 4.00315, -0.59416])
+        # self.truthPoints['17'] = np.array([1.35135, -0.99257, -0.41729])
+        # self.truthPoints['18'] = np.array([4.00388, 4.02418, 0.08610])
+        # self.truthPoints['19'] = np.array([1.15202, 1.01983, -0.72926])
+        # self.truthPoints['21'] = np.array([4.09984, 1.55906, -0.58504])
+        # self.truthPoints['22'] = np.array([2.52925, 1.73592, -0.97918])
+        # self.truthPoints['23'] = np.array([2.15135, 3.04855, -0.73227])
+
+        # MOCAP 2025 Sept AprilTags
+        self.truthPoints['3'] = np.array([-0.12710, -1.21415, -1.23755])
+        self.truthPoints['5'] = np.array([0.70693, -1.90905, -0.70546])
+        self.truthPoints['8'] = np.array([1.38529, 2.16612, -1.24269])
+        self.truthPoints['9'] = np.array([5.35155, -1.39477, 0.29776])
+        self.truthPoints['13'] = np.array([1.98385, -0.65895, -1.24558])
+        self.truthPoints['15'] = np.array([3.14300, 1.31598, -0.78247])
+        self.truthPoints['16'] = np.array([3.12187, 0.52254, -0.65637])
+        self.truthPoints['18'] = np.array([3.49844, 0.51306, -0.23403])
+        self.truthPoints['19'] = np.array([2.07009, -1.72274, -0.69668])
+        self.truthPoints['20'] = np.array([3.92250, -3.10489, 0.24589])
+        self.truthPoints['21'] = np.array([3.12467, -0.07760, -1.24881])
+        self.truthPoints['22'] = np.array([1.67668, -0.00762, -1.70942])
+        self.truthPoints['23'] = np.array([2.16908, -1.33895, -0.46741])
 
     def getTruthPointsDict(self):
         return self.truthPoints
